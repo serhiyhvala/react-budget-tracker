22227b50188410df640b06821b29568777e106cd
serhiy hvala <serhiyhvaladew@gmail.com>
2022-09-08 14:02:53 +0300

    UPD: remaining budget controller

--- src/components/Remaining.jsx
+++ src/components/Remaining.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-
+import React, {useContext} from 'react'
+import { AppContext } from '../context/AppContext'
 const Remaining = () => {
+  const {expenses, budget} = useContext(AppContext)
+  const totalExpenses = expenses.reduce((total, item) => {
+    return (total = total + item.cost)
+  }, 0);
+
+  const alertType = totalExpenses > budget ? 'alert-danger' : 'alert-success'
+
   return (
-    <div className="alert alert-success">
+    <div className={`alert ${alertType}`}>
         <span>
-            Remaining: $1200
+            Remaining: ${budget - totalExpenses}
         </span>
     </div>
   )
